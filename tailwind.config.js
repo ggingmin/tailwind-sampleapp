197c50a632833674ea7a9ff1f4f0ee8176dfc3bf
ggingmin <ggingmin@gmail.com>
2022-05-14 16:57:00 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        'blue': '#0439D9',
+        'skyblue': '#1fb6ff',
+        'blue-light': '#0D51D9',
+        'green-pale': '#80BFA8',
+        'green-light': '#7e5bef',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      }
+    },
+  },
+  plugins: [],
+}
